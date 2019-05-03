f67f464ffc1dd045c7e534f9401f63f51eed739c
Rafael Andrade <rafael.andrade@aviva.com.br>
2019-05-03 16:43:17 -0300

    add project

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { FramePageComponent } from './pages/master/frame.page';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent,
+    PetsPageComponent,
+    ProductsPageComponent,
+    CartPageComponent,
+    FramePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/Services/data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-products-page',
+  templateUrl: './products-page.component.html'
+})
+export class ProductsPageComponent implements OnInit {
+  public products$: Observable<any[]>;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.products$ = this.data.getProducts();
+  }
+
+}
