64c315f5a88ccb404e2ff3981a7f3bcd011f44e8
Rafael Andrade <rafael.andrade@aviva.com.br>
2020-01-24 16:50:28 -0300

    fix auth service

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -4,16 +4,16 @@ import { Security } from '../utils/security.util';
 
 @Injectable()
 export class AuthService implements CanActivate {
-    constructor(private router: Router) {
+  constructor(private router: Router) {
+  }
+
+  canActivate() {
+    const token = Security.getToken();
+    if (!token) {
+      this.router.navigate(['/login']);
+      return false;
     }
 
-    canActivate() {
-        const token = Security.getToken();
-        if (!token) {
-            this.router.navigate(['/login']);
-            return false;
-        }
-
-        return true;
-    }
+    return true;
+  }
 }
