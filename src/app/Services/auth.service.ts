a8cba3df4bf0624dfad864a2c70262ed0a79d456
Rafael Andrade <rafael.andrade@aviva.com.br>
2020-01-24 16:49:30 -0300

    update config editor, auth services

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { Security } from '../utils/security.util';
 
